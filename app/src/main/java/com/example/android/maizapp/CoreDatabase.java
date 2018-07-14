500af0dd20afaad23f0eed8b75e37752fd984007
Ahmed Yousef <dixterboy@gmail.com>
2018-07-14 03:49:00 +0200

    Rearranged code structure and delete unimportant comments.

--- app/src/main/java/com/example/android/maizapp/CoreDatabase.java
+++ app/src/main/java/com/example/android/maizapp/CoreDatabase.java
@@ -4,15 +4,8 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
-
-/**
- * This class creates the database of the Maiz groups.
- * @author Ahmed Yousef
- */
 
 public class CoreDatabase extends SQLiteOpenHelper {
-
 
     final static private String DB_NAME = "MaizApp";
     final static private String DB_MAIZ_TABLE = "Maiz";
@@ -20,39 +13,36 @@ public class CoreDatabase extends SQLiteOpenHelper {
     final static private String DB_PURCHASE_TABLE = "Purchase";
     final static private int DB_VER = 4;
 
-    private Context context;
     private SQLiteDatabase mSQLiteDatabase;
-    private Cursor mCursor;
-
-    /**
-     * This constructs the database to the SQLiteOpenHelper - super class - to create the database.
-     * @param context the context of current state of the application/object.
-     */
 
     public CoreDatabase(Context context) {
         super(context, DB_NAME, null, DB_VER);
-        this.context = context;
     }
-
-    /**
-     *This method creates the tables of the database.
-     * @param sqLiteDatabase executes SQLite queries.
-     */
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table " + DB_MAIZ_TABLE + "( _id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, MaizName TEXT, MemberCount INTEGER);");
-        sqLiteDatabase.execSQL("create table " + DB_MEMBER_TABLE + "( _id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, MaizID INTEGER, MemberName TEXT,  TotalPaid REAL, FOREIGN KEY (MaizID) REFERENCES Maiz(_id));");
-        sqLiteDatabase.execSQL("create table " + DB_PURCHASE_TABLE + "( _id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, MemberID INTEGER, MaizID INTEGER, PurchasesName TEXT, Price REAL, FOREIGN KEY (MemberID) REFERENCES Member(_id), FOREIGN KEY (MaizID) REFERENCES Maiz(_id));");
-        Log.i("CoreDatabase", "Table Created");
+        sqLiteDatabase.execSQL("create table " + DB_MAIZ_TABLE +
+                                "( _id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                                " MaizName TEXT," +
+                                " MemberCount INTEGER);");
+
+        sqLiteDatabase.execSQL("create table " + DB_MEMBER_TABLE +
+                                "( _id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                                " MaizID INTEGER," +
+                                " MemberName TEXT," +
+                                " TotalPaid REAL," +
+                                " FOREIGN KEY (MaizID) REFERENCES Maiz(_id));");
+
+        sqLiteDatabase.execSQL("create table " + DB_PURCHASE_TABLE +
+                                "( _id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                                " MemberID INTEGER," +
+                                " MaizID INTEGER," +
+                                " PurchasesName TEXT," +
+                                " Price REAL," +
+                                " FOREIGN KEY (MemberID) REFERENCES Member(_id)," +
+                                " FOREIGN KEY (MaizID) REFERENCES Maiz(_id));");
     }
 
-    /**
-     * This method drops the tables of the database if the version of the database changed
-     * @param sqLiteDatabase executes SQLite queries.
-     * @param i
-     * @param i1
-     */
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DB_MAIZ_TABLE);
@@ -61,40 +51,32 @@ public class CoreDatabase extends SQLiteOpenHelper {
         onCreate(sqLiteDatabase);
     }
 
-    /**
-     * Executes inserting query to insert data to database
-     * @param maizName refers to Maiz name
-     * @param memberCount refers to member count in the Maiz
-     */
     public void insertDataMaizTable(String maizName, String memberCount) {
         mSQLiteDatabase = getWritableDatabase();
-        mSQLiteDatabase.execSQL("INSERT INTO " + DB_MAIZ_TABLE + " (MaizName, MemberCount) VALUES('" + maizName + "','" + memberCount + "');");
+        mSQLiteDatabase.execSQL("INSERT INTO " + DB_MAIZ_TABLE +
+                                " (MaizName, MemberCount) " +
+                                " VALUES('" + maizName + "','" + memberCount + "');");
     }
 
-    /**
-     * Executes inserting query to insert data to database
-     * @param maizID refers to MaizID which is a foreign key in Member table.
-     * @param memberName refers to member name.
-     */
     public void insertDataMembersTable(int maizID, String memberName) {
         mSQLiteDatabase = getWritableDatabase();
-        mSQLiteDatabase.execSQL("INSERT INTO  " + DB_MEMBER_TABLE + " (MaizID, MemberName) VALUES('" + maizID + "','" + memberName + "');");
+        mSQLiteDatabase.execSQL("INSERT INTO  " + DB_MEMBER_TABLE +
+                                " (MaizID, MemberName) " +
+                                " VALUES('" + maizID + "','" + memberName + "');");
     }
 
-    /**
-     * Executes inserting query to insert data to database
-     * @param memberID references to MemberID which is a foreign key in Purchase table.
-     * @param purchaseName refers to purchase name.
-     * @param price refers to price name.
-     */
     public void insertDataPurchaseTable(int memberID, int maizID, String purchaseName, String price) {
         mSQLiteDatabase = getWritableDatabase();
-        mSQLiteDatabase.execSQL("INSERT INTO " + DB_PURCHASE_TABLE + " (MemberID, MaizID, PurchasesName, Price) values('" + memberID + "','" + maizID + "','" + purchaseName + "', '" + price +"');");
+        mSQLiteDatabase.execSQL("INSERT INTO " + DB_PURCHASE_TABLE +
+                                " (MemberID, MaizID, PurchasesName, Price) " +
+                                " VALUES('" + memberID + "','" + maizID + "','" + purchaseName + "', '" + price +"');");
     }
 
     public int readFromMaizTable(int currentMaizID){
         mSQLiteDatabase = getReadableDatabase();
-        mCursor = mSQLiteDatabase.rawQuery("SELECT MemberCount FROM Maiz WHERE _id = '"+ currentMaizID +"'", null);
+        Cursor mCursor = mSQLiteDatabase.rawQuery("SELECT MemberCount " +
+                                                        "FROM Maiz " +
+                                                        "WHERE _id = '"+ currentMaizID +"'", null);
         mCursor.moveToFirst();
         int memberCount = mCursor.getInt(0);
         mCursor.close();
@@ -103,49 +85,50 @@ public class CoreDatabase extends SQLiteOpenHelper {
 
     public void updateDataMaizTable(int instantMemberCount, int currentMaizID){
         mSQLiteDatabase = getWritableDatabase();
-        mSQLiteDatabase.execSQL("UPDATE Maiz SET MemberCount = '" + instantMemberCount + "' WHERE _id = '" + currentMaizID + "' ");
+        mSQLiteDatabase.execSQL("UPDATE Maiz" +
+                                " SET MemberCount = '" + instantMemberCount + "' " +
+                                " WHERE _id = '" + currentMaizID + "' ");
     }
 
     public void deleteFromMaizTable(String maizName){
         mSQLiteDatabase = getWritableDatabase();
-        mSQLiteDatabase.execSQL("DELETE FROM Maiz WHERE MaizName = '"+ maizName +"'");
+        mSQLiteDatabase.execSQL("DELETE FROM Maiz " +
+                                "WHERE MaizName = '"+ maizName +"'");
     }
 
     public void deleteFromMembersTable(String memberName, int currentMaizID){
         mSQLiteDatabase = getWritableDatabase();
-        mSQLiteDatabase.execSQL("DELETE FROM Member WHERE MemberName = '" + memberName + "' AND  MaizID = '" + currentMaizID + "' ");
+        mSQLiteDatabase.execSQL("DELETE FROM Member " +
+                                "WHERE MemberName = '" + memberName + "' AND  MaizID = '" + currentMaizID + "' ");
     }
 
     public void deleteFromMembersTable(int currentMaizID){
         mSQLiteDatabase = getWritableDatabase();
-        mSQLiteDatabase.execSQL("DELETE FROM Member WHERE MaizID = '" + currentMaizID + "' ");
+        mSQLiteDatabase.execSQL("DELETE FROM Member " +
+                                "WHERE MaizID = '" + currentMaizID + "' ");
     }
 
-    /**
-     * Executes deleting query to delete data from database
-     * @param memberName refers to member name.
-     */
     public void deleteFromPurchaseTable(String memberName, int currentMaizID){
         mSQLiteDatabase = getWritableDatabase();
-        mSQLiteDatabase.execSQL("DELETE FROM " + DB_PURCHASE_TABLE + " WHERE MemberID IN (SELECT _id FROM Member WHERE MemberName = '" + memberName +"' AND MaizID = '"+ currentMaizID +"')");
+        mSQLiteDatabase.execSQL("DELETE FROM " + DB_PURCHASE_TABLE + " " +
+                                "WHERE MemberID " +
+                                "IN (SELECT _id FROM Member " +
+                                    "WHERE MemberName = '" + memberName +"' " +
+                                    "AND MaizID = '"+ currentMaizID +"')");
     }
 
     public void deleteFromPurchaseTable(int currentMaizID){
         mSQLiteDatabase = getWritableDatabase();
-        mSQLiteDatabase.execSQL("DELETE FROM " + DB_PURCHASE_TABLE + " WHERE MaizID = '" + currentMaizID + "'");
+        mSQLiteDatabase.execSQL("DELETE FROM " + DB_PURCHASE_TABLE + "" +
+                                " WHERE MaizID = '" + currentMaizID + "'");
     }
 
-    /**
-     * Executes deleting query to delete data from database
-     */
     public void deleteAllPurchases(int maizID){
         mSQLiteDatabase = getWritableDatabase();
-        mSQLiteDatabase.execSQL("DELETE FROM " + DB_PURCHASE_TABLE + " WHERE MaizID = '"+ maizID +"'");
+        mSQLiteDatabase.execSQL("DELETE FROM " + DB_PURCHASE_TABLE +
+                                " WHERE MaizID = '"+ maizID +"'");
     }
 
-    /**
-     * Closes database if it's opened.
-     */
     public void closeDB() {
         SQLiteDatabase db = this.getReadableDatabase();
         if (db != null && db.isOpen())
